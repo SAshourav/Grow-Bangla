ea533e8f173edc818c612d61d2f26900a68155e6
Sabbir Ahmed <sabbirahmed2013303@gmail.com>
2023-03-21 17:33:04 +0600

    Forgot Password and verify the email

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,16 +1,13 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './Login.css';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signInWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
 import app from '../firebase/firebase.init';
 
 const auth = getAuth(app);
 const Login = () => {
     const [user , setUser] = useState({});
     const navigate = useNavigate();
-    const loginHandler = () => {
-        navigate('/home');
-    }
     const signupHandler = () =>{
       navigate('/signupC')
     }
@@ -26,22 +23,61 @@ const Login = () => {
           console.error(error);
         })
     }
+    const [success, setSuccess] = useState(false);
+    const [userEmail, setUserEmail] = useState('');
+    const handleLogin = (event) => {
+      event.preventDefault();
+      setSuccess(false);
+      const form = event.target;
+      const email = form.email.value;
+      const password = form.password.value;
+      console.log(email, password);
+
+      signInWithEmailAndPassword(auth, email, password)
+        .then(result => {
+          const user = result.user;
+          console.log(user);
+          setSuccess(true);
+        })
+        .catch(error => {
+          console.error(error);
+        })
+    }
+    const handleEmailBlur = (event) => {
+      const email = event.target.value;
+      setUserEmail(email);
+      console.log(email);
+    }
+    const handleForgotPassword = () => {
+      if(!userEmail){
+        alert("Please enter your email");
+        return;
+      }
+      sendPasswordResetEmail(auth, userEmail)
+        .then( () => {
+          alert('Password reset email sent successfully');
+        })
+        .catch(error => {
+          console.error(error);
+        })
+    }
   return (
     <div className="login-container">
       <h3>Welcome Back To Grow Bangla !</h3>
       <h2>Login</h2>
-      <form>
-        <label htmlFor="username">E-mail</label>
-        <input type="text" id="username" name="username" required />
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" required />
-        <button onClick={loginHandler} type="submit">Login</button>
-        <p>Sign-in with Other Services</p>
-        <p onClick={handleGoogleSignIn} className='button-icon'>Google</p>
-        <Link className='forgot' to='/forgotPage'>Forgot Password ?</Link>
-        <p>Or , Create An Account</p>
-        <button onClick={signupHandler}>SignUp</button>
-      </form>
+        <form onSubmit={handleLogin} className='signUpForm'>
+            <label htmlFor="email">Email</label>
+            <input onBlur={handleEmailBlur} type="email" id="email" name="email" required />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" required />
+            <button type="submit">Sign-In</button>
+            <p>Sign-in with Other Services</p>
+            <p onClick={handleGoogleSignIn} className='button-icon'>Google</p>
+            <button onClick={handleForgotPassword} type='button' className='btn link-btn'>Forgot Password?</button>
+            <p>Or , Create An Account</p>
+            <button onClick={signupHandler}>SignUp</button>
+          </form>
+      
     </div>
   );
 }
--- src/Component/SignUpC/SignUpC.js
+++ src/Component/SignUpC/SignUpC.js
@@ -1,24 +1,69 @@
-import React from 'react';
-import {createUserWithEmailAndPassword, getAuth} from "firebase/auth";
+import React, { useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile} from "firebase/auth";
 import './SignUpC.css';
 import app from '../firebase/firebase.init';
 
 const auth = getAuth(app);
 const SignUpC = () => {
+    const [passwordError, setPasswordError] = useState('');
+    const [success, setSuccess] = useState(false);
     const handleRegistration = (event) => {
         event.preventDefault();
-        const email = event.target.email.value;
-        const password = event.target.password.value;
+        
+        setSuccess(false);
+
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
         console.log(email, password);
+
+        //Validating password
+
+        if(!/(?=.*[A-Z].*[A-Z])/.test(password)){
+            setPasswordError('Please Provide at least two upper case characters');
+            return;
+        }
+        if(password.length < 8){
+            setPasswordError('Password must be at least 8 characters');
+            return;
+        }
+        setPasswordError('');
 
         createUserWithEmailAndPassword(auth, email, password)
             .then((result)=> {
                 const user = result.user;
                 console.log(user);
+                setSuccess(true);
+                form.reset();
+                verifyEmail();
+                updateUserName(name)
             })
             .catch((error) => {
                 console.error(error);
+                setPasswordError(error.message);
             });
+    }
+
+    const verifyEmail = () => {
+        sendEmailVerification(auth.currentUser)
+            .then(() => {
+                alert("Please Check your email and verify");
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    }
+    const updateUserName = (name) => {
+        updateProfile(auth.currentUser, {
+            displayName: name
+        })
+            .then(() => {
+                console.log("name updated");
+            })
+            .catch((error) => {
+                console.error(error);
+            })
     }
     return (
         <div className='body'>
@@ -26,11 +71,13 @@ const SignUpC = () => {
                 <h2>Create an Account</h2>
                 <form onSubmit={handleRegistration} className='signUpForm'>
                     <label htmlFor="full-name">Full Name</label>
-                    <input type="text" id="full-name" name="full-name" required />
+                    <input type="text" id="name" name="name" required />
                     <label htmlFor="email">Email</label>
                     <input type="email" id="email" name="email" required />
                     <label htmlFor="password">Password</label>
                     <input type="password" id="password" name="password" required />
+                    {success && <p className='text-success'>User Created Successfully !!</p>}
+                    <p className='text-danger'>{passwordError}</p>
                     <button type="submit">Sign Up</button>
                 </form>
                 </div>
